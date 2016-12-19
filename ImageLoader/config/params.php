30c2be5a9c44f8bea672e98da4c000f57e20962e
Oleksii Prykhodko <kalimdor07@gmail.com>
2016-12-19 13:29:13 +0200

    comment added

--- ImageLoader/config/params.php
+++ ImageLoader/config/params.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * The file contains configs for the package
+ *
+ *
  */
 
 return [
     'allowedExtensions' => ['image/gif', 'image/png', 'image/jpeg'],
-    'path' => 'images'
+    'path' => 'images'              //create this folder where your index.php is located
 ]
 ?>
